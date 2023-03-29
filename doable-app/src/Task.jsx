02fac2aa4f5dc2d9732d4c1f3cc91610295d3e65
clarence <clarencejames@live.com.au>
2023-03-30 00:13:51 +1100

    checked kinda works

--- doable-app/src/Task.jsx
+++ doable-app/src/Task.jsx
@@ -3,16 +3,19 @@ import React from "react";
 import { FaRegTrashAlt } from "react-icons/fa";
 
 const style = {
-
+    li: `flex justify-between items-center p-2 my-2 bg-slate-200 rounded-md`,
+    liChecked: `flex justify-between items-center p-2 my-2 bg-slate-200 rounded-md line-through`,
+    task: `flex justify-between items-center`,
+    
 }
 
 export default function Task({ task }) {
 
     return (
-    <li className={style.li}>
+    <li className={task.completed ? style.liChecked : style.li}>
         <div className={style.task}>
-            <input type="checkbox" className={style.checkbox} />
-            <p className={style.taskName}>{task.text}</p>
+            <input type="checkbox" className={style.checkbox} checked={task.completed ? 'checked' : ''} />
+            <p className={task.completed ? style.textComplete : style.text}>{task.text}</p>
             <button className={style.deleteButton}><FaRegTrashAlt /></button>
         </div>
     </li>
