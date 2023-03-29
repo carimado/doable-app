e66ec84e437edb3b70e7f9e49be361720ea0eda2
clarence <clarencejames@live.com.au>
2023-03-30 00:22:20 +1100

    styles

--- doable-app/src/Task.jsx
+++ doable-app/src/Task.jsx
@@ -3,10 +3,14 @@ import React from "react";
 import { FaRegTrashAlt } from "react-icons/fa";
 
 const style = {
-    li: `flex justify-between items-center p-2 my-2 bg-slate-200 rounded-md`,
-    liChecked: `flex justify-between items-center p-2 my-2 bg-slate-200 rounded-md line-through`,
-    task: `flex justify-between items-center`,
-    
+    li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+    liChecked: `flex justify-between p-4 my-2 bg-slate-200 capitalize`,
+    checkbox: `cursor-pointer`,
+    task: `flex`,
+    text: `ml-2 text-slate-900`,
+    textChecked: `ml-2 text-slate-900 line-through`,
+    deleteButton: `cursor-pointer flex items-center`,
+
 }
 
 export default function Task({ task }) {
@@ -15,9 +19,9 @@ export default function Task({ task }) {
     <li className={task.completed ? style.liChecked : style.li}>
         <div className={style.task}>
             <input type="checkbox" className={style.checkbox} checked={task.completed ? 'checked' : ''} />
-            <p className={task.completed ? style.textComplete : style.text}>{task.text}</p>
-            <button className={style.deleteButton}><FaRegTrashAlt /></button>
+            <p className={task.completed ? style.textChecked : style.text}>{task.text}</p>
         </div>
+        <button className={style.deleteButton}><FaRegTrashAlt /></button>
     </li>
     )
 }
